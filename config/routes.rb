4ff12f768dc7b9fa218669875d772f46c5e96ff8
Anton Gordon <anton.gordon85@outlook.com>
2022-02-19 15:15:23 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :day, only: [:index, :show]
+  resources :task
+  resources :mood, only: [:index, :new, :edit, :show]
 end
