dfbe8fb85635bf3e40596a18f4a72bde4f1f2cb8
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-02-24 21:26:49 +0000

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :day, only: [:index, :show]
-  resources :task
-  resources :mood, only: [:index, :new, :edit, :show]
+  resources :days, only: [:index, :show]
+  resources :tasks
+  resources :moods, only: [:index, :new, :edit, :show]
 end
