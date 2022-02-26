58135ea7d333a52b7ee6f38d5cb0884cbdbd3a28
Arham Zubair <arhamzubair00@gmail.com>
2022-02-26 15:58:03 +0000

    Working on the moods index page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,19 @@
 
 require 'faker'
 
-user = User.create!(:firstname => Faker::Name.first_name, :lastname => Faker::Name.last_name, :email => Faker::Internet.email, :password => 'topsecret', :password_confirmation => 'topsecret')
+
+  user1 = User.create!(:firstname => Faker::Name.first_name, :lastname => Faker::Name.last_name, :email => Faker::Internet.email, :password => 'topsecret', :password_confirmation => 'topsecret')
+  day1 = Day.create!(:calendar_date => Date.today, :user_id => user1.id )
+  happy_mood = Mood.create!(mood_name: 'Happy', day: day1 )
+  task1 = Task.create!(title: 'Get out of bed', day: day1)
+
+  user2 = User.create!(:firstname => Faker::Name.first_name, :lastname => Faker::Name.last_name, :email => Faker::Internet.email, :password => 'topsecret', :password_confirmation => 'topsecret')
+  day2 = Day.create!(:calendar_date => Date.today, :user_id => user2.id )
+  sad_mood = Mood.create!(mood_name: 'Sad', day: day2 )
+  task2 = Task.create!(title: 'Make some coffee', day: day2)
+
   # if user.save
   #   puts "User added"
   # else
   #   puts "Failed"
   # end
-
-day = Day.create!(:calendar_date => Date.today, :user_id => user.id )
-# day2 = Day.create!(:calendar_date => Date.today, :user_id => user2.id )
-
-
-happy_mood = Mood.create!(mood_name: 'Happy', day: day )
-sad_mood = Mood.create!(mood_name: 'Sad', day: day )
-
-task1 = Task.create!(title: 'Get out of bed', day: day)
