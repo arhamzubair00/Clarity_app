212bd7f9ebd296c8f9a0d52bc1898eb8e6f14390
Arham Zubair <arhamzubair00@gmail.com>
2022-03-01 20:15:47 +0000

    Moods index page styling done. Showing three separate boxes for date, notes and emoji. Data only coming from the seed file

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,13 +1,7 @@
 class Mood < ApplicationRecord
-  # mood_name is the emoji
-  # moods =  ['Angry', 'Sad', 'Meh', 'Neutral', 'Happy', 'Excited']
-  # moods_emojis =  [ '😡', '😢', '😟', '😐', '🙂', '😍' ]
-  # mood_notes is the notes the user may or may not add
-
   belongs_to :day
   validates :mood_notes, length: { maximum: 350 }, allow_blank: true
   validates :mood_name, presence: true
   validates :day_id, presence: true, uniqueness: true
   # validates :mood_name, :inclusion { :in => [ '😡', '😢', '😟', '😐', '🙂', '😍' ] }
-
 end
