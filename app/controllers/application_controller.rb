72c342797cb876bc3a4f8a41a836c1504dda2eb4
Arham Zubair <arhamzubair00@gmail.com>
2022-03-03 17:43:27 +0000

    Logic for after_sign_in_path_for now working.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,13 +11,15 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     @current_day = Day.where(calendar_date: Date.today)
-    @mood = Mood.where(day_id: @current_day)
-    raise
-    # if @mood.mood_name.empty?
-    #  redirect_to days_path
-    # # # else
-    # # # days_path
-    # end
+    @mood = Mood.where(day_id: @current_day, mood_name: "#")
+
+    # raise
+
+    if @mood.blank?
+      days_path
+    else
+      edit_mood_path(find_mood_params)
+    end
 
     # days_path
     # @current_day = Day.where("calendar_date == #{Date.today}")
@@ -28,6 +30,12 @@ class ApplicationController < ActionController::Base
     # else
     #   days_path
     # end
+  end
+
+  private
+
+  def find_mood_params
+    @mood.as_json.first["id"]
   end
 
 end
