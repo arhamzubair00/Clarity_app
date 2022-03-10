58470880a397bfd94d8ac15bb6581c019eee40ce
Anton Gordon <anton.gordon85@outlook.com>
2022-03-10 21:42:06 +0000

    Split Show css into seperate files for buttons, pomodoro, new task; Updated New task to work with ajax and errors;

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -4,12 +4,12 @@ class DaysController < ApplicationController
   def index
     # @days = Day.all
     @days = Day.select { |day| day.user_id == 1} # "2" should be replaced by some kind of user validation from devise.
-    @days.sort_by { |day| day.calendar_date }
+    @days.order(:calendar_date) # use .order (active record) bc .sort_by is ruby code and the object is and active record reference
   end
 
   def show
     @day = Day.find(params[:id])
-    @tasks = Task.select { |task| task.day == @day}
+    @tasks = Task.where(day: @day)
     @task = Task.new
     # @mood = Mood.find { |mood| mood.day == @day}
   end
