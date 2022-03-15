7d5f6509869e9ae462b96cfa99c88c4cc5df9dad
Anton Gordon <anton.gordon85@outlook.com>
2022-03-15 18:44:32 +0000

    some stuff to stash

--- app/javascript/components/pomodoro.js
+++ app/javascript/components/pomodoro.js
@@ -4,23 +4,38 @@ export const initTimer = () => {
   const shortBreakBtn = document.getElementById("short-break-btn")
   const longBreakBtn = document.getElementById("long-break-btn")
   const startBtn = document.getElementById("start-btn")
-  const pauseBtn = document.getElementById("pause-btn")
-  const colors = ['#456FAB', '#EF9D82']
 
   let timeDisplay = document.getElementById("time-display")
   let timeout;
-  let index = 0;
 
-  workBtn.addEventListener("click", function () { startTimer(25 * 60) })
-  longBreakBtn.addEventListener("click", function () { startTimer(15 * 60) })
-  shortBreakBtn.addEventListener("click", function () { startTimer(5 * 60) })
+  workBtn.addEventListener("click", function () {
+    startTimer(25 * 60)
+    workBtn.style.opacity = "1"
+    longBreakBtn.style.opacity = "0.5"
+    shortBreakBtn.style.opacity = "0.5"
+  })
+
+  longBreakBtn.addEventListener("click", function () {
+    startTimer(15 * 60)
+    workBtn.style.opacity = "0.5"
+    longBreakBtn.style.opacity = "1"
+    shortBreakBtn.style.opacity = "0.5"
+  })
+
+  shortBreakBtn.addEventListener("click", function () {
+    startTimer(5 * 60)
+    workBtn.style.opacity = "0.5"
+    longBreakBtn.style.opacity = "0.5"
+    shortBreakBtn.style.opacity = "1"
+  })
 
   function startTimer(time) {
     if (typeof timeout !== undefined) {
       clearInterval(timeout)
     }
     let allowedTime = time
-    let isPaused = false
+    let isPaused = true
+    startBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 512 512"><title>ionicons-v5-c</title><path fill="#456fab" d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48Zm74.77,217.3L216.32,334.44A10.78,10.78,0,0,1,200,325.13V186.87a10.78,10.78,0,0,1,16.32-9.31L330.77,246.7A10.89,10.89,0,0,1,330.77,265.3Z"/></svg>`
     showTime(time)
     timeout = setInterval(function () {
       if (allowedTime == 0) {
@@ -53,36 +68,14 @@ export const initTimer = () => {
 
     startBtn.addEventListener("click", function (e) {
       e.preventDefault();
-      isPaused = false;
+      isPaused = !isPaused;
+
+      if (isPaused) {
+        startBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 512 512"><title>ionicons-v5-c</title><path fill="#456fab" d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48Zm74.77,217.3L216.32,334.44A10.78,10.78,0,0,1,200,325.13V186.87a10.78,10.78,0,0,1,16.32-9.31L330.77,246.7A10.89,10.89,0,0,1,330.77,265.3Z"/></svg>`
+      } else {
+        startBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 512 512"><title>ionicons-v5-c</title><path fill="#456fab" d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48ZM224,320a16,16,0,0,1-32,0V192a16,16,0,0,1,32,0Zm96,0a16,16,0,0,1-32,0V192a16,16,0,0,1,32,0Z"/></svg>`
+      }
     })
-
-    pauseBtn.addEventListener("click", function (e) {
-      e.preventDefault();
-      isPaused = true;
-    })
-
-
-
-    // pauseBtn.addEventListener("click", function onClick() {
-    //   pauseBtn.style.backgroundColor = 'salmon';
-    //   pauseBtn.style.Color = 'white';
-    //   isPaused = true;
-    // })
-
-    startBtn.addEventListener('click', function onClick() {
-      startBtn.style.backgroundColor = colors[index];
-      startBtn.style.color = 'white';
-
-      index = index >= colors.length - 1 ? 0 : index + 1;
-    })
-
-    pauseBtn.addEventListener('click', function onClick() {
-      pauseBtn.style.backgroundColor = colors[index];
-      pauseBtn.style.color = 'white';
-
-      index = index >= colors.length - 1 ? 0 : index + 1;
-    })
-
   }
 
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,7 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+
 import { renderCalendar } from '../components/calendar';
 import { initTimer } from '../components/pomodoro';
 
@@ -39,14 +40,13 @@ document.addEventListener('turbolinks:load', () => {
 
   // initSelect2();
 
+
   if (workBtn){
     initTimer();
   }
   if (calendar){
-    console.log("calendar");
     renderCalendar();
   }
-
 
 
 });
